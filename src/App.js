e66a711b0cd219097daac80506d013e76e0479ba
rako <milosz@rako.dev>
2020-12-31 05:16:00 +0000

    privacy page router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import About from './pages/about';
 import UploadPage from './pages/upload';
+import PrivacyPage from './pages/privacy';
 
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
       <Switch>
         <Route path="/about">
           <About />
+        </Route>
+        <Route path="/privacy">
+          <PrivacyPage/>
         </Route>
         <Route path="/">
           <UploadPage/>
