3b31e0a5a694740a3226b20da643e104288a24ed
rako <milosz@rako.dev>
2020-12-31 05:15:51 +0000

    added the privacy page, plugged it up to back end

---
+++ src/pages/privacy.js
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+const axios = require("axios").default;
+const { Container, Col, Button, Modal, Alert } = require("react-bootstrap");
+
+var RequestType = {
+    DELETE: 1,
+    ANON: 2
+}
+
+function PrivacyPage() {
+    const [modalShown, setShow] = useState(false);
+    const [showAlert, setAlertShow] = useState(false);
+    const [alertType, setAlertType] = useState();
+
+    function showModal() {
+        setShow(true);
+    }
+
+    function closeModal() {
+        setShow(false);
+    }
+
+    async function processRequest(requestType) {
+        closeModal()
+
+        const instance = axios.create({baseURL: "http://localhost:4000"})
+
+        switch (requestType) {
+            case (RequestType.DELETE):
+                try{
+                    await instance.delete("statistics/delete")
+                } catch(err) {
+                    setAlertType("danger")
+                    setAlertShow(err.response.data)
+                    return;
+                }
+
+                setAlertType("success")
+                setAlertShow("Successfully deleted all the data related to this IP address!")
+                break;
+            case (RequestType.ANON):
+                try{
+                    await instance.post("statistics/anonymize")
+                } catch(err) {
+                    setAlertType("danger")
+                    setAlertShow(err.response.data)
+                    return;
+                }
+
+                setAlertType("success")
+                setAlertShow("Successfully anonymised your data!")
+                break;
+            default:
+                break;
+        }
+    }
+
+    return (
+        <Container>
+            <Alert show={showAlert != false} variant={alertType} dismissible onClick={() => setAlertShow(false)}>
+                <Alert.Heading>{alertType == "success" ? "Success!" : "Failure!"}</Alert.Heading>
+
+                <p>{showAlert}</p>
+            </Alert>
+
+            <Col>
+                <h4>Privacy Options</h4>
+                <p>Here you're able to either delete all of your data or make it anonymous by requesting to delete the identifying IP from any of records related to you.</p>
+                <p>This is a <strong>instant</strong> action and it cannot be reversed. The data is used for statistical purposes only.</p>
+
+                <Modal show={modalShown} onHide={()=>setShow(false)}>
+                    <Modal.Header closeButton >
+                        Are you sure?
+                        </Modal.Header>
+
+                    <Modal.Body>
+                        This action cannot be reversed! Thread lightly, samurai!
+                        </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button onClick={() => processRequest(RequestType.DELETE)}>
+                            I am sure!
+                            </Button>
+                    </Modal.Footer>
+                </Modal>
+
+                <div>
+                    <Button variant="danger" onClick={() => showModal()} >Delete Data</Button>
+                    <Button variant="warning" className="ml-1" onClick={() => processRequest(RequestType.ANON)}>Anonymise Data</Button>
+                </div>
+            </Col>
+        </Container>
+    )
+}
+
+export default PrivacyPage;
