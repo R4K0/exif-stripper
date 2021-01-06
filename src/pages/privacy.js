e48a66559a6228bf799507359f30bef8a4000b25
rako <milosz@rako.dev>
2021-01-06 19:46:32 +0000

    more comments

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -2,17 +2,20 @@ import { useEffect, useState } from 'react';
 const axios = require("axios").default;
 const { Container, Col, Button, Modal, Alert } = require("react-bootstrap");
 
+// Used as an ENUM for request types
 var RequestType = {
     DELETE: 1,
     ANON: 2
 }
 
+// Privacy page
 function PrivacyPage() {
     const [modalShown, setShow] = useState(false);
     const [showAlert, setAlertShow] = useState(false);
     const [alertType, setAlertType] = useState();
     const [recordAmount, setRecords] = useState(undefined);
 
+    // Upon the page load, we fetch statistics count related to us.
     function fetchCount(){
         axios.get('http://localhost:4000/statistics/count').then(response => {
             if (response?.data?.count !== undefined) {
@@ -35,6 +38,7 @@ function PrivacyPage() {
         setShow(false);
     }
 
+    // this handles sending out the request to the server for anonymizing or deleting our data
     async function processRequest(requestType) {
         closeModal()
 
@@ -48,8 +52,10 @@ function PrivacyPage() {
                     setAlertType("danger")
                     setAlertShow(err.response?.data || "An error occured :(")
                     return;
+                    // if error occured, the code below won't execute.
                 }
 
+                // set alerts depending on outcome.
                 setAlertType("success")
                 setAlertShow("Successfully deleted all the data related to this IP address!")
                 setRecords(0)
@@ -85,6 +91,7 @@ function PrivacyPage() {
                 <p>Here you're able to either delete all of your data or make it anonymous by requesting to delete the identifying IP from any of records related to you.</p>
                 <p>This is an <strong>instant</strong> action and it cannot be reversed. The data is used for statistical purposes only.</p>
                 {
+                    // Display the record amount about the user only if we managed to fetch it
                     recordAmount !== undefined ? <div><hr /><p>There are {recordAmount} records related to your IP in our database.</p></div> : undefined
                 }
 
